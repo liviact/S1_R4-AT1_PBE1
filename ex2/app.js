16c7dad80db96d23cfce51589eb8249c2ea5844b
unknown <lilicaetano19@gmail.com>
2025-08-19 14:51:33 -0300

    feito ex2

---
+++ ex2/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const PORT = 8081;
+
+// GET /calculadora?operacao=soma&numUm=4&numDois=6
+app.get("/calculadora", (req, res) => {
+    try {
+        const { operacao, primeiroNumero, segundoNumero } = req.query;
+
+        if (primeiroNumero == undefined || primeiroNumero == "" || isNaN(primeiroNumero) || segundoNumero == undefined || segundoNumero == "" || isNaN(segundoNumero)) {
+
+            return res.status(400).send(`Campos obrigatórios não preenchidos ou inválidos!`);
+        }
+        let resultado = 0;
+        switch (operacao) {
+            case "soma":
+                resultado = parseFloat(primeiroNumero) + parseFloat(segundoNumero);
+                break;
+            case "subtracao":
+                resultado = parseFloat(primeiroNumero) - parseFloat(segundoNumero);
+                break;
+            case "multiplicacao":
+                resultado = parseFloat(primeiroNumero) * parseFloat(segundoNumero);
+                break;
+                
+            case "divisao":
+                if ((primeiroNumero = !0) && (segundoNumero = !0)) {
+                    resultado = parseFloat(primeiroNumero) / parseFloat(segundoNumero);
+                } else {
+                    return res.status(400).send(`operação inválida!`);
+                }
+                break;
+
+            default:
+                return res.status(400).send(`nenhuma operação válida!`);
+        }
+
+
+
+        res.status(200).send(`<h1>Olá, o resultado da conta é ${resultado} </h1>`);
+    } catch (error) {
+        console.error("Erro capturado:", error);
+        res.status(500).send(`Erro interno no servidor`);
+    }
+});
+app.listen(PORT, function mensagem() { // toda função tem um parêntese e chavinha
+    console.log(`servidor rodando em https://localhost:${PORT}`)
+});
